07e0ad72db75e6245e3e2945b392f15098da491f
Andreas Nauleau <anauleau@gmail.com>
2013-03-10 19:27:18 -0700

    adds poster nack to tooltip and other enhancements

--- src/bubbles.js
+++ src/bubbles.js
@@ -2,10 +2,8 @@ var bubbles = (function() {
 
 //NEED TO EDIT --->>
   var urls = {      //API urls
-      // news  : "http://hndroidapi.appspot.com/news/format/json/page/?appid=vishna&callback=?",
-      news: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=kkd937tfu53qzuesmc68j99k&limit=31&callback=?"
+      boxOfficeCritics: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=kkd937tfu53qzuesmc68j99k&limit=36&callback=?"
   },
-      thread = /^(item[?]id[=][0-9]+)/,               //regexp for HN thread posts
       w = $(window).width() * 0.85,  //width
       h = $(window).height() * 0.85, //height
       m = 20,                                         //margin
@@ -70,23 +68,24 @@ var bubbles = (function() {
       }
   }
 //d is equal to the object that is returned by the ajax request
-//url is urls.news
-//NEED TO EDIT --->>
+//url is urls.new
   function load( url, callback ){
       $.getJSON(url, function( data ) {
         posts = data.movies;
         next = posts.pop();
 
-        console.log(posts);
         var rank = 0;
         posts.map( function(d) {
-          var comments = d.critics_consensus,
-              score    = parseInt( d.ratings.critics_score ),
-              time     = d.release_dates.theater.split(" ").toString(),
-              format   = d3.time.format("%Y-%m-%d");
+          var comments    = d.critics_consensus,
+              score       = parseInt( d.ratings.critics_score ),
+              time        = d.release_dates.theater.split(" ").toString(),
+              format      = d3.time.format("%Y-%m-%d")
+              scoreCritic = d.ratings.critics_score,
+              mpaaRating  = d.mpaa_rating,
+              scoreAud    = d.ratings.audience_score;
               rank ++;
+
           d.rank = rank;
-          console.log(d.rank);
           if (comments === undefined){
             d.comments = '<i>No critical consensus available.<i>';
           } else {
@@ -95,6 +94,24 @@ var bubbles = (function() {
           d.score = score;
           d.time = format.parse(time);
           d.url = d.links.alternate;
+          d.color = '#024C68';
+          // switch (mpaaRating){
+          //   case 'R':
+          //     d.color = '#055959';
+          //     break;
+          //   case 'PG-13':
+          //     d.color = '#130C68';
+          //     break;
+          //   case 'PG':
+          //     d.color = '#957408';
+          //     break;
+          //   case 'G':
+          //     d.color = '#954808';
+          //     break;
+          //   default:
+          //     d.color = '#430763';
+          //     break;
+          // }
           return d;
         });
 
@@ -120,11 +137,9 @@ var bubbles = (function() {
         callback();
       });
   }
-//<<-----
-
 
 var moveText = function(d){
-    d.text.attr('x', d.x)
+    d.text && d.text.attr('x', d.x)
           .attr('y', d.y)
 };
 
@@ -156,23 +171,28 @@ function launch() {
     .attr("cx", function(d) { return d.x; })
     .attr("cy", function(d) { return d.y; })
     .attr("fill", function(d) { return z( d.comments ); })
-    .attr("stroke-width", 2)
+    // .attr("stroke-width", 2)
     .attr("stroke", function(d) { return d3.rgb(z( d.comments )).darker(); })
     .attr("id", function(d) { return "post_#" + d.item_id; })
     .attr("title", function(d) { return d.title; })
     .style("opacity", function(d) { return o( d.time ); })
-    .on("mouseover", function(d, i) { force.resume(); highlight( d, i, this ); })
-    .on("mouseout", function(d, i) { downlight( d, i, this ); }).each(function(d, i){
-    d.text = svg.append('text').attr('x', d.x)
-                        .attr('y', d.y)
-                        .text(d.rank)
-    });
+    .attr("fill", function(d){return d.color;})
+    .on("mouseover", function(d, i) { highlight( d, i, this ); })
+    .on("mouseout", function(d, i) { downlight( d, i, this ); });
 
   d3.selectAll("circle")
     .transition()
-    .delay(function(d, i) { return i * 10; })
+    .ease('bounce')
+    .delay(function(d, i) { return i * 50; })
     .duration( 1000 )
-    .attr("r", function(d) { return r( d.score ); });
+    .attr("r", function(d) { return r( d.score ); })
+    .each('end', function(d, i){
+    d.text = svg.append("text").attr("x", d.x)
+                        .attr("y", d.y)
+                        .text(d.rank)
+                        .attr("font-size", "36px")
+                        .attr("font-family", '"Amatic SC", cursive')
+    });
 
   loadGravity( moveCenter );
 
@@ -185,6 +205,7 @@ function launch() {
         .on("tick", function(e) {
           generator(e.alpha);
           d = node
+
             .attr("cx", function(d) { return d.x;})
             .attr("cy", function(d) { return d.y; })
             .data()
@@ -207,9 +228,9 @@ function legend() {
     .append("linearGradient")
     .attr("id", "legendGradient")
     .attr("x1", "0%")
-    .attr("y1", "0%")
+    .attr("y1", "100%")
     .attr("x2", "0%")
-    .attr("y2", "100%")
+    .attr("y2", "0%")
     .attr("spreadMethod", "pad");
 
   linearGradient
@@ -239,13 +260,13 @@ function legend() {
     .append("text")
     .attr("x", 45)
     .attr("y", 30)
-    .text("Oldest");
+    .text("Newest");
 
   legend
     .append("text")
     .attr("x", 45)
     .attr("y", 220)
-    .text("Newest");
+    .text("Oldest");
 
 }
 
@@ -253,12 +274,14 @@ function legend() {
 //appends url for hover state
 function highlight( data, i, element ) {
   d3.select( element ).attr( "stroke", "black" );
+  // d3.select(".thumb").attr("src", data.posters.detailed);
   var description = data.comments,
-    content = '<span class=\"title\"><a href=\"' + data.url + '\">' + data.title + '</a></span><br/>' +
-               description + "<br/>" + '<a href="' + data.url + '"><img src="' + data.posters.detailed + '" alt="alt text" style="border:none;" /></a>';
-
-
-
+      scoreAud    = data.ratings.audience_score,
+      scoreCritic = data.ratings.critics_score,
+          content = '<span class=\"title\"><a href=\"' + data.url + '\">' + data.title + '</a></span><br/>' +
+               description + "<br/>" + '<span><b>Critical Score:</b> ' + scoreCritic + '</span>' +
+               '<br><span><b>Audience Score:</b> ' + scoreAud + '</span>' +
+               '<a href="' + data.url + '"><img src="' + data.posters.detailed + '" alt="alt text" style="border:none;" /></a>';
   tooltip.showTooltip(content, d3.event);
 }
 
@@ -270,11 +293,11 @@ function downlight( data, i, element ) {
 $("a.category").on("click", function(e) { update( $(this).attr("value") ); });
 
 return {
-  categories : ["news", "movies"],
+  categories : ["boxOfficeCritics", "movies"],
   init : init,
   update : update
 };
 
 })();
 
-bubbles.init( window.location.href.split("#")[1] ? window.location.href.split("#")[1] : "news");
+bubbles.init( window.location.href.split("#")[1] ? window.location.href.split("#")[1] : "boxOfficeCritics");
